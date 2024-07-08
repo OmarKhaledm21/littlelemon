b59b0f9e190846f187ca191a466293a78d06e293
Omarkhaledm21 <omarkhaledm21@gmail.com>
2024-07-08 16:25:32 +0300

    ADD: new RESTAPIs for models and Added admin config

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,11 @@
-from django.contrib import admin 
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
-  
-urlpatterns = [ 
-    path('', views.index, name='index')
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,50 @@
 from django.shortcuts import render
+from . import serializers
+from .models import Booking, Menu
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    serializer_class = serializers.BookingSerializer
+    queryset = Booking.objects.all()
+
+
+"""
+class MenuView(APIView):
+    def get(self,request):
+        menus = Menu.objects.all()
+        serializer = serializers.MenuSerializer(menus, many=True)
+        return Response(serializer.data)
+
+class BookingView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request):
+        bookings = Booking.objects.all()
+        serializer = serializers.BookingSerializer(bookings, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = serializers.BookingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+"""
