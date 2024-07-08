efadc00bf83c1a1dc10251faa43a671ccf7352f7
Omarkhaledm21 <omarkhaledm21@gmail.com>
2024-07-08 15:18:18 +0300

    ADD: models for restaurant app

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Menu, Booking
 
-# Register your models here.
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'inventory')
+    verbose_name = 'Menu'
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'no_of_guests', 'booking_date')
+    verbose_name = 'Booking'
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(99999)]
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(
+        validators=[MaxValueValidator(999999)]
+    )
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
